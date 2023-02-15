827bd2ad74850c2a8286e237a9372016d48f7424
ozgebudak <44921289+ozgebudak@users.noreply.github.com>
2023-02-15 22:07:18 +0300

    projem

---
+++ FlaskServer/FlaskServer/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_geek():
+    return '<h1>Flask Server</h2>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0",port=5001)
